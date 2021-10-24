ebbf86660869cb6cf7a8417080944558bf340fdd
maxwen <maxwen@users.noreply.github.com>
2021-10-24 07:59:56 +0000

    Build OctaviLab into Settings
    Signed-off-by: Sagarrokade006 <sagarrokade006@gmail.com>
    Signed-off-by: Pratyaksh Bharadwaj <pratyakshom.05@gmail.com>

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -324,4 +324,7 @@ public class Settings extends SettingsActivity {
      * Activity for AppDashboard.
      */
     public static class AppDashboardActivity extends SettingsActivity {}
+
+    // OctaviLab Additoon
+    public static class OctaviLabActivity extends SettingsActivity {}
 }
